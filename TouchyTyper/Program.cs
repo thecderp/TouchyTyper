5418ccdbe6c5650c864067fa4fc88de6894922c0
thecderp <70461970+thecderp@users.noreply.github.com>
2020-10-23 20:25:19 +1100

    Teh big dump
    It may be a  smart move to connect this to git on visual studio.

---
+++ TouchyTyper/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading;
+
+namespace TouchyTyper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNum = 0;
+            int maxNum = 26;
+            int[] matchyText = new int[10]; //10 elements
+            Random randNum = new Random();
+            for (int i = 0; i < matchyText.Length; i++) {
+                matchyText[i] = randNum.Next(minNum, maxNum);
+                Console.WriteLine(matchyText[i]);
+            }
+            
+
+
+           /*
+           * int Min = 0;
+           * int Max = 20;
+           * 
+           * // this declares an integer array with 5 elements
+           * // and initializes all of them to their default value
+           * // which is zero
+           * int[] test2 = new int[5];
+           * 
+           * Random randNum = new Random();
+           * for (int i = 0; i < test2.Length; i++)
+           * {
+           *     test2[i] = randNum.Next(Min, Max);
+           * }
+           */
+        }
+    }
+}
