965314a691e7b09253c4361552f7d2cc053f4c73
thecderp <thelinuxderp@gmail.com>
2020-10-24 14:03:27 +1100

    Saving this before i break everything

--- TouchyTyper/Program.cs
+++ TouchyTyper/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace TouchyTyper
 {
@@ -10,9 +10,10 @@ namespace TouchyTyper
     {
         static void Main(string[] args)
         {
-            int minNum = 0;
+            int minNum = 1;
             int maxNum = 26;
             int[] matchyText = new int[10]; //10 elements
+            
             Random randNum = new Random();
             for (int i = 0; i < matchyText.Length; i++) {
                 matchyText[i] = randNum.Next(minNum, maxNum);
@@ -21,3 +22,4 @@ namespace TouchyTyper
         }
     }
 }
+
